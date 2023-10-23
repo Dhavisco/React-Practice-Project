821f15d4e684d98c1a35ef6f44164f363baa5df7
Dhavisco <olaniyidavid2002@gmail.com>
2023-10-23 15:28:04 +0100

    Added condition to not display card when there is no user

--- src/components/Users/UsersList.jsx
+++ src/components/Users/UsersList.jsx
@@ -2,6 +2,12 @@ import Card from '../UI/Card'
 import classes from './UsersList.module.css'
 
 const UsersList = (props) => {
+
+  // Check if the users list is empty
+  if (props.users.length === 0) {
+    return;
+  }
+
   return (
     <Card className={classes.users}>
       <ul>
